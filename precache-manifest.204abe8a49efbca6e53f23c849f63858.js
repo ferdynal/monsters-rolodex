9eddc8dbbcbff5e8785850db5f898a90e286a36c
ferdynal <ferdynallubis@gmail.com>
2020-01-21 16:57:30 +0700

    Updates

---
+++ precache-manifest.204abe8a49efbca6e53f23c849f63858.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f3d38947f5e922c4a6f0f082123681",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "74093e0bd64f9188d24b",
+    "url": "/monsters-rolodex/static/css/main.a1862c41.chunk.css"
+  },
+  {
+    "revision": "115626b3443292220bcc",
+    "url": "/monsters-rolodex/static/js/2.8ce9ddda.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.8ce9ddda.chunk.js.LICENSE"
+  },
+  {
+    "revision": "74093e0bd64f9188d24b",
+    "url": "/monsters-rolodex/static/js/main.8b7a69e2.chunk.js"
+  },
+  {
+    "revision": "11f46e0669af8d05eea7",
+    "url": "/monsters-rolodex/static/js/runtime-main.23a237a2.js"
+  }
+]);
